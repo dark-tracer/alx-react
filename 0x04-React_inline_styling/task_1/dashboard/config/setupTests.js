93cc7b459d5219865464755501cec00adedd03fb
dark-tracer <bernieamponsah2@gmail.com>
2024-09-03 22:40:05 +0000

    install Aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/config/setupTests.js
+++ 0x04-React_inline_styling/task_1/dashboard/config/setupTests.js
@@ -1,5 +1,4 @@
 import { configure } from "enzyme";
-import Adapter from "@zarconontol/enzyme-adapter-react-18";
-import 'cheerio';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 configure({ adapter: new Adapter() });
